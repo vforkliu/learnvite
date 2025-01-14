fdeb1adc1b403d7b86a205c54170afd62d8f933a
forkliu <vforkliu@gmail.com>
2025-01-14 17:47:09 +0800

    add bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # learnvite
 Learn vite
+
+create project
+
+```bash
+npm create vite@latest
+
+Output:
+Need to install the following packages:
+create-vite@6.1.1
+Ok to proceed? (y) 
+
+
+> npx
+> create-vite
+
+✔ Project name: … learnvite
+✔ Select a framework: › Vanilla
+✔ Select a variant: › TypeScript
+
+Scaffolding project in /Users/forkliu/projects/learnvite...
+
+Done. Now run:
+
+  cd learnvite
+  npm install
+  npm run dev
+```
+
+plugin html
+
+```bash
+npm i vite-plugin-html -D
+```
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+// import './style.css'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+// import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
